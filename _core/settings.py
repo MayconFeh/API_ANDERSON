bd7d2516164756bd475bc06befac8010d2635ca8
MayconFeh <mfbm87@gmail.com>
2024-04-22 12:00:21 -0300

    add mores apps(comments, logs, order, products, rating)

--- _core/settings.py
+++ _core/settings.py
@@ -190,8 +190,6 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-AUTH_USER_MODEL = "accounts.Account"
-
 SPECTACULAR_SETTINGS = {
     'TITLE': 'Anderson_API',
     'DESCRIPTION': 'API for Anderson project',
