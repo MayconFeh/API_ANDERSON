8de98102995090babbef4d00e48a32da44bdc652
MayconFeh <mfbm87@gmail.com>
2024-05-09 09:41:35 -0300

    urls fix

--- _core/urls.py
+++ _core/urls.py
@@ -15,12 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, includes
+from django.urls import path, include
 from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', includes('accounts.urls')),
+    path('api/', include('accounts.urls')),
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
     path("api/docs/redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"),
     path('api/docs/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
