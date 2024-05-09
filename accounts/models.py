eebc4fc9c15ea8cb17f8ca265d3bc1340716298d
MayconFeh <mfbm87@gmail.com>
2024-05-09 09:01:03 -0300

    accouts(models) update, user_cards(models) finished

--- accounts/models.py
+++ accounts/models.py
@@ -2,12 +2,10 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 from uuid import uuid4
 
-USERTYPE_CHOICES = (('USR', 'User'), ('ADM', 'Admin'))
-
 
 class Account(AbstractUser):
     id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
-    userType = models.CharField(max_length=3, choices=USERTYPE_CHOICES, default=USERTYPE_CHOICES[0])
+    is_superuser = models.BooleanField(default=False)
     name = models.CharField(max_length=50)
     username = models.CharField(max_length=50, unique=True)
     email = models.EmailField(max_length=100, unique=True)
